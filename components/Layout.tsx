d3f83300a2e39a51aec5c77b6dd2c48f4027f5a7
piggydoughnut <piggydoughnut@gmail.com>
2022-11-13 13:54:23 +0800

    added layout, logo to go home and basic menu

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,32 @@
-const Navbar = () => <div></div>;
+import { Button } from "@material-tailwind/react";
+import Link from "next/link";
+
+const Navbar = () => (
+  <div className="flex justify-between items-center mx-20 pt-4">
+    <Link
+      className="font-bold hover:text-indigo-400 transition-all hover:ease-in-out"
+      href="/"
+    >
+      Hack The Cycle
+    </Link>
+    <div className="flex flex-row gap-12 items-center">
+      <Link
+        className="hover:underline hover:underline-offset-4 hover:decoration-2 transition-all hover:ease-in-out"
+        href="/about"
+      >
+        About
+      </Link>
+      <Link href="/signup">
+        <Button
+          variant="outlined"
+          className=" w-36 h-11 uppercase text-sm font-plusJakarta"
+        >
+          Sign Up
+        </Button>
+      </Link>
+    </div>
+  </div>
+);
 const Footer = () => <div></div>;
 
 export default function Layout({
---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <div></div>
+      <h1>About me</h1>
+    </Layout>
+  );
+}
