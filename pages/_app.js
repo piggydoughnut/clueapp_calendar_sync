e1aea48e09ae7af545ee0f285c0cf88bf030ceff
piggydoughnut <piggydoughnut@gmail.com>
2023-01-18 12:17:33 +0800

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import "/styles/globals.css";
 
+import ReactGA from "react-ga4";
 import { ThemeProvider } from "@material-tailwind/react";
 
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA);
+ReactGA.send("pageview");
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
