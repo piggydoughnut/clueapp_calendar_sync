a8e0c56a373c1a20c2b667cd2f69bb95184355d0
piggydoughnut <piggydoughnut@gmail.com>
2023-04-16 15:56:59 +1200

    added line height to the calendar email for readability

--- helpers/templates/calendar.ts
+++ helpers/templates/calendar.ts
@@ -16,8 +16,8 @@ const calendar = compile(
     <mj-section>
       <mj-column>
         <mj-text font-size="16px" color="#342525" font-family="helvetica" padding-top="60px">Hello there dear human!</mj-text>
-        <mj-text font-size="16px" color="#342525" font-family="helvetica">Here is your personalized cycle calendar. It will guide you through your month. </mj-text>
-        <mj-text font-size="16px" color="#342525" font-family="helvetica">Everyone's experience with their menstrual cycle is unique, and it's important to honor your own needs and preferences. Remember, it's okay to listen to your body and adjust your routine as needed, and self-care looks different for everyone.</mj-text>
+        <mj-text line-height="20px" font-size="16px" color="#342525" font-family="helvetica">Here is your personalized cycle calendar to guide you through your month. </mj-text>
+        <mj-text line-height="24px" font-size="16px" color="#342525" font-family="helvetica">Everyone's experience with their menstrual cycle is unique, and it's important to honor your own needs and preferences. Remember, it's okay to listen to your body and adjust your routine as needed, and self-care looks different for everyone.</mj-text>
       </mj-column>
     </mj-section>
     <mj-section padding-bottom="100px" padding-top="50px">
@@ -33,13 +33,13 @@ const calendar = compile(
 
           <p style="font-size: 18px; text-transform: uppercase; font-family: helvetica;">Self-care time</p>
         </mj-text>
-        <mj-text color="#342525" font-family="helvetica" font-size="16px">Take it easy and prioritize self-care activities that help you feel relaxed and nurtured during this time of physical and emotional release. Listen to your body and give yourself permission to slow down and rest. The way you spend this phase will dictate how the rest of your month goes.</mj-text>
+        <mj-text  line-height="24px" color="#342525" font-family="helvetica" font-size="16px">Take it easy and prioritize self-care activities that help you feel relaxed and nurtured during this time of physical and emotional release. Listen to your body and give yourself permission to slow down and rest. The way you spend this phase will dictate how the rest of your month goes.</mj-text>
         <mj-text color="#342525" font-family="helvetica" font-size="16px">
           Best course of action <br />
           <ul>
-            <li>Focus on self-love and to conserve energy for the next cycle.</li>
-            <li>Listen to your intuition and think about the past month, reflect on your experiences, and emotions.</li>
-            <li>Consider how you feel about the progress you have made, and what you want to achieve in the next cycle.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Focus on self-love and to conserve energy for the next cycle.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Listen to your intuition and think about the past month, reflect on your experiences, and emotions.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Consider how you feel about the progress you have made, and what you want to achieve in the next cycle.</li>
           </ul>
         </mj-text>
         <mj-text align="center" padding-top="60px">
@@ -50,13 +50,13 @@ const calendar = compile(
           <p style="font-size: 18px; text-transform: uppercase; font-family: helvetica;">Ready to tackle the world</p>
         </mj-text>
 
-        <mj-text color="#342525" font-family="helvetica" font-size="16px">You're feeling renewed and motivated, so take advantage of this energy to set goals and build healthy habits.</mj-text>
+        <mj-text color="#342525"   line-height="24px" font-family="helvetica" font-size="16px">You're feeling renewed and motivated, so take advantage of this energy to set goals and build healthy habits.</mj-text>
         <mj-text color="#342525" font-family="helvetica" font-size="16px">
           Best course of action <br />
           <ul>
-            <li>Plan and set goals for the upcoming weeks and months.</li>
-            <li>Schedule important meetings or events during this time.</li>
-            <li>Tackle new challenges.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Plan and set goals for the upcoming weeks and months.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Schedule important meetings or events during this time.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Tackle new challenges.</li>
           </ul>
         </mj-text>
 
@@ -70,13 +70,13 @@ const calendar = compile(
         </mj-text>
 
 
-        <mj-text color="#342525" font-family="helvetica" font-size="16px">Your communication skills and focus are heightened during this time, so use this opportunity to tackle challenging projects and make new connections. Remember to take breaks and nurture yourself, as pushing too hard can lead to burnout.</mj-text>
+        <mj-text color="#342525"   line-height="24px" font-family="helvetica" font-size="16px">Your communication skills and focus are heightened during this time, so use this opportunity to tackle challenging projects and make new connections. Remember to take breaks and nurture yourself, as pushing too hard can lead to burnout.</mj-text>
         <mj-text color="#342525" font-family="helvetica" font-size="16px">
           Best course of action <br />
           <ul>
-            <li>Prioritize challenging or creative work projects that require focus and attention.</li>
-            <li>Take advantage of heightened communication skills to initiate conversations or negotiations. This is great time for job interviews, salary negotiations or any other important communication.</li>
-            <li>Connect with others, network, and attend social events to make new connections.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Prioritize challenging or creative work projects that require focus and attention.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">This is a great time for job interviews, salary negotiations or any other important communication.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Connect with others, network, and attend social events to make new connections.</li>
           </ul>
         </mj-text>
 
@@ -88,14 +88,14 @@ const calendar = compile(
           <p style="font-size: 18px; text-transform: uppercase; font-family: helvetica;">TIME TO SLOW DOWN</p>
         </mj-text>
 
-        <mj-text color="#342525" font-family="helvetica" font-size="16px">It's important to be gentle with yourself during this time of fluctuating emotions and physical discomfort. Remember, this is a natural part of your cycle and will pass in due time.</mj-text>
+        <mj-text   line-height="24px" color="#342525" font-family="helvetica" font-size="16px">It's important to be gentle with yourself during this time of fluctuating emotions and physical discomfort. Remember, this is a natural part of your cycle and will pass in due time.</mj-text>
         <mj-text color="#342525" font-family="helvetica" font-size="16px">
           Best course of action <br />
           <ul>
-            <li>Practice self-compassion and avoid over-scheduling or taking on too much work.</li>
-            <li>Incorporate relaxation techniques like meditation or deep breathing to reduce stress and anxiety.</li>
-            <li>Reconsider attending busy and socially strenuous events.</li>
-            <li>Prioritize activities that promote rest and rejuvenation, such as getting enough sleep, spending time in nature, and taking warm baths.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Avoid over-scheduling or taking on too much work.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Practice meditation or deep breathing to reduce stress and anxiety.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Reconsider attending busy and socially strenuous events.</li>
+            <li style="padding-bottom: 8px; line-height: 22px">Prioritize getting enough sleep, spending time in nature, and taking warm baths.</li>
           </ul>
         </mj-text>
 
