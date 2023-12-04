8e82bfe75936ad06ccb790261423930274c34377
piggydoughnut <piggydoughnut@gmail.com>
2023-12-04 11:53:22 +0800

    privacy policy update

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -18,90 +18,121 @@ export default function PrivacyPolicyPage() {
         <PageHeader>Privacy Policy</PageHeader>
         <div className="max-w-[600px]">
           <p>Effective Date: 5.11.2023</p>
-          <H2> 1. Introduction</H2>
-          <p>
-            This Privacy Policy explains how Hack Your Cycle (&quot;we,&quot;
-            &quot;us,&quot; &quot;our&quot;) collects, uses, and protects your
-            personal information when you use our services, including our
-            menstrual cycle tracking and productivity tool. We are committed to
-            protecting your privacy and ensuring the security of your data.
+          <H2>Policy Summary</H2>
+          <p className="my-4">
+            Personal Data is collected for the following purposes and using the
+            following services:
           </p>
-          <H2> 2. Information We Collect</H2>
-          <H3>a. Google Email Address</H3>
-          <p>
-            When you use our service, we collect your Google email address in
-            order to send you very occasional emails related to our product,
-            updates, and important notifications.
-          </p>
-          <H3>b. Google Tokens</H3>
-          <p>
-            We store your Google tokens to facilitate the seamless updating of
-            your calendar without requiring you to repeatedly authorize our
-            access to your Google Calendar. These tokens help improve the user
-            experience.
-          </p>
-          <H3>c. Google Calendar Id</H3>
-          <p>
-            We store the Google Calendar Id associated with your
-            MyCycle-generated calendar. This information is used to ensure the
-            proper functioning of our service and to provide you with accurate
-            menstrual cycle and productivity information.
-          </p>
-          <H2> 3. How We Use Your Information</H2>
-          <p>We use the collected information for the following purposes:</p>
-          <H3>a. Sending Occasional Emails</H3>
-          <p>
-            We use your Google email address to send you very occasional emails,
-            such as product updates and important notifications related to our
-            service.
-          </p>
-          <H3>b. Calendar Integration</H3>
-          <p>
-            Your Google tokens and CalendarId are used to integrate and interact
-            with your Google Calendar. This allows you to efficiently manage
-            your menstrual cycle-related events and productivity goals.
-          </p>
-          <H2> 4. Data Security</H2>
-          <p>
-            We take the security of your data seriously and implement reasonable
-            safeguards to protect it from unauthorized access, disclosure,
-            alteration, or destruction. However, please be aware that no data
-            transmission over the internet or data storage system is 100%
-            secure, and we cannot guarantee the absolute security of your
-            information.
-          </p>
-          <H2> 5. Data Retention</H2>
-          <p>
-            We retain your data for as long as necessary to fulfill the purposes
-            outlined in this Privacy Policy. You may request the removal of your
-            data by contacting us at {emailUs}.
-          </p>
-          <H2>6. Sharing Your Information</H2>
-          <p>
-            We do not share your personal information with third parties, except
-            when required by law or as described in this Privacy Policy.{" "}
-          </p>
-          <H2>7. Your Rights</H2>
-          You have the right to: - Access the personal information we hold about
-          you. - Correct any inaccuracies in your personal information. - Delete
-          your personal information. - Object to the processing of your personal
-          information.
-          <p>
-            To exercise these rights or for any questions regarding your data,
-            please contact us at {emailUs}.
-          </p>
-          <H2>8. Changes to this Privacy Policy</H2>
-          <p>
-            We may update this Privacy Policy to reflect changes in our data
-            practices or legal requirements. The updated Privacy Policy will be
-            posted on our website with the revised effective date.
-          </p>
-          <H2>9. Contact Us</H2>
-          <p>
-            If you have any questions or concerns about this Privacy Policy or
-            our data practices, please contact us at {emailUs}.
-          </p>
-          <p className="mt-8">Yours, Hack Your Cycle Team.</p>
+          <ul className="list-disc list-inside flex flex-col gap-2">
+            <li>
+              Google auth tokens: We store your Google tokens for hassle-free
+              user experience by avoiding repeated authorizations.
+            </li>
+
+            <li>
+              Google calendar id: We store a newly generated Google Calendar Id
+              to ensure proper service functionality.
+            </li>
+
+            <li>
+              Google email address: Send you very occasional emails related to
+              our product, updates, and important notifications.
+            </li>
+          </ul>
+          <div className="mt-10">
+            <H2>Full Policy</H2>
+            <H2> 1. Introduction</H2>
+            <p>
+              This Privacy Policy explains how Hack Your Cycle (&quot;we,&quot;
+              &quot;us,&quot; &quot;our&quot;) collects, uses, and protects your
+              personal information when you use our services. We are committed
+              to protecting your privacy and ensuring the security of your data.
+              Hack Your Cycle use and transfer to any other app of information
+              received from Google APIs will adhere to{" "}
+              <LinkTo
+                url="https://developers.google.com/terms/api-services-user-data-policy?"
+                name=" Google API Services User Data"
+              ></LinkTo>
+              Policy, including the Limited Use requirements.
+            </p>
+            <H2> 2. Information We Collect</H2>
+            <H3>a. Google Email Address</H3>
+            <p>
+              When you use our service, we collect your Google email address in
+              order to send you very occasional emails related to our product,
+              updates, and important notifications.
+            </p>
+            <H3>b. Google Tokens</H3>
+            <p>
+              We store your Google tokens to facilitate the seamless updating of
+              your calendar without requiring you to repeatedly authorize our
+              access to your Google Calendar. These tokens help improve the user
+              experience.
+            </p>
+            <H3>c. Google Calendar Id</H3>
+            <p>
+              We store the Google Calendar Id associated with your
+              MyCycle-generated calendar. This information is used to ensure the
+              proper functioning of our service and to provide you with accurate
+              menstrual cycle and productivity information.
+            </p>
+            <H2> 3. How We Use Your Information</H2>
+            <p>We use the collected information for the following purposes:</p>
+            <H3>a. Sending Occasional Emails</H3>
+            <p>
+              We use your Google email address to send you very occasional
+              emails, such as product updates and important notifications
+              related to our service.
+            </p>
+            <H3>b. Calendar Integration</H3>
+            <p>
+              Your Google tokens and CalendarId are used to integrate and
+              interact with your Google Calendar. This allows you to efficiently
+              manage your menstrual cycle-related events and productivity goals.
+            </p>
+            <H2> 4. Data Security</H2>
+            <p>
+              We take the security of your data seriously and implement
+              reasonable safeguards to protect it from unauthorized access,
+              disclosure, alteration, or destruction. However, please be aware
+              that no data transmission over the internet or data storage system
+              is 100% secure, and we cannot guarantee the absolute security of
+              your information.
+            </p>
+            <H2> 5. Data Retention</H2>
+            <p>
+              We retain your data for as long as necessary to fulfill the
+              purposes outlined in this Privacy Policy. You may request the
+              removal of your data by contacting us at {emailUs}.
+            </p>
+            <H2>6. Sharing Your Information</H2>
+            <p>
+              We do not share your personal information with third parties,
+              except when required by law or as described in this Privacy
+              Policy.{" "}
+            </p>
+            <H2>7. Your Rights</H2>
+            You have the right to: - Access the personal information we hold
+            about you. - Correct any inaccuracies in your personal information.
+            - Delete your personal information. - Object to the processing of
+            your personal information.
+            <p>
+              To exercise these rights or for any questions regarding your data,
+              please contact us at {emailUs}.
+            </p>
+            <H2>8. Changes to this Privacy Policy</H2>
+            <p>
+              We may update this Privacy Policy to reflect changes in our data
+              practices or legal requirements. The updated Privacy Policy will
+              be posted on our website with the revised effective date.
+            </p>
+            <H2>9. Contact Us</H2>
+            <p>
+              If you have any questions or concerns about this Privacy Policy or
+              our data practices, please contact us at {emailUs}.
+            </p>
+            <p className="mt-8">Yours, Hack Your Cycle Team.</p>
+          </div>
         </div>
       </div>
     </Layout>
